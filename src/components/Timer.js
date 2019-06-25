277404708e98123bce8e1b6c531fe1ddbbcdf01d
drillprops <drylbartosz@gmail.com>
2019-06-25 21:01:17 +0200

    create Timer component

---
+++ src/components/Timer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { secondFont } from '../utils/fonts';
+import { secondary } from '../utils/colors';
+
+const TimerWrapper = styled.section`
+  grid-row: 3;
+  text-align: center;
+  grid-column: 3/4;
+`;
+const TimerFaze = styled.h3`
+  display: center;
+  font-family: ${secondFont};
+  color: ${secondary};
+  font-size: 2rem;
+  margin: 0;
+`;
+const Time = styled.h1`
+  display: center;
+  font-size: 6rem;
+  margin: 0;
+  line-height: 0.7em;
+`;
+
+const Timer = () => {
+  return (
+    <TimerWrapper>
+      <TimerFaze>interval</TimerFaze>
+      <Time>30:00</Time>
+    </TimerWrapper>
+  );
+};
+
+export default Timer;
