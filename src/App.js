1c6c0b894b13e6ca537a4c699225fd43e035d222
drillprops <drylbartosz@gmail.com>
2019-06-25 20:30:23 +0200

    add Interface to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './utils/GlobalStyle';
+import Interface from './components/Interface';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Interface />
     </>
   );
 };
