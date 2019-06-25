7e820a6dd630eb11d1cb2a8af1e3486bed8b38c5
drillprops <drylbartosz@gmail.com>
2019-06-25 20:53:27 +0200

    set height to 100vh

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -6,6 +6,7 @@ const InterfaceWrapper = styled.main`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: repeat(6, 1fr);
+  height: 100vh;
 `;
 
 const Interface = () => {
