a3cde44a09317d4ea306272dc8ce329d5b703c61
drillprops <drylbartosz@gmail.com>
2019-07-10 19:30:32 +0200

    add seconds and timeString to initialState, create Timer interface

--- src/duck/reducers.ts
+++ src/duck/reducers.ts
@@ -1,5 +1,11 @@
-const initialState: object = {
-  hello: 'hello'
+interface Timer {
+  seconds: number;
+  timeString: string;
+}
+
+const initialState: Timer = {
+  seconds: 0,
+  timeString: ''
 };
 
 export default (state = initialState) => {
