eadabbe55fc9af985d64afff9ff40584a0435e8b
drillprops <drylbartosz@gmail.com>
2019-07-10 22:27:29 +0200

    replace usestate with redux selector and dispatch

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import moment = require('moment');
+import { useDispatch, useSelector } from 'react-redux';
+import { setTimer } from '../duck/actions';
 
 const useTimer = (inputValue = 0): [string, number, (num: number) => void] => {
-  const [seconds, setSeconds] = useState(inputValue);
+  const dispatch = useDispatch();
+  const seconds = useSelector((state: any) => state.seconds);
 
   const convertSeconds = (seconds: number): string => {
     const timerDurationSeconds = moment
@@ -25,8 +28,9 @@ const useTimer = (inputValue = 0): [string, number, (num: number) => void] => {
   const timeAsString = convertSeconds(seconds);
 
   const updateTimer = (num: number) => {
-    if (num >= 0) setSeconds(num);
-    else setSeconds(0);
+    if (num >= 0) {
+      dispatch(setTimer(num));
+    } else dispatch(setTimer(0));
   };
 
   return [timeAsString, seconds, updateTimer];
