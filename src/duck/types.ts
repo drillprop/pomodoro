465a159079d9f6b19a38e7efe57555bd62386eec
drillprops <drylbartosz@gmail.com>
2019-07-10 19:30:03 +0200

    add four action types

---
+++ src/duck/types.ts
@@ -0,0 +1,6 @@
+const SET_TIMER: string = 'SET_TIMER';
+const START_TIMER: string = 'START_TIMER';
+const PAUSE_TIMER: string = 'PAUSE_TIMER';
+const STOP_TIMER: string = 'PAUSE_TIMER';
+
+export { SET_TIMER, START_TIMER, PAUSE_TIMER, STOP_TIMER };
