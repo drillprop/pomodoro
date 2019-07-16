35248c6ae57bb8bcec33338324a758aa11a394a3
drillprops <drylbartosz@gmail.com>
2019-07-16 21:37:34 +0200

    change viewBox settings

--- src/elements/Icon.tsx
+++ src/elements/Icon.tsx
@@ -9,7 +9,7 @@ const Icon = ({ name, color }: IconProps) => {
   switch (name) {
     case 'play':
       return (
-        <svg width='50' height='30' viewBox='0 0 30 34'>
+        <svg width='60' height='60' viewBox='-12 -12 55 55'>
           <defs />
           <path
             d='M16.661,8.708a5.549,5.549,0,0,1,10.055,0l11.1,20.62c2.4,4.458-.426,10.178-5.028,10.178H10.59c-4.6,0-7.427-5.721-5.028-10.178Z'
