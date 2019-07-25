58ceedd2fe32abeee18a89094120acc24086f842
drillprops <drylbartosz@gmail.com>
2019-07-25 22:01:45 +0200

    clean up styled component mess

--- src/components/Timer/Category.tsx
+++ src/components/Timer/Category.tsx
@@ -3,40 +3,48 @@ import styled from 'styled-components';
 import { secondFont } from '../../utils/fonts';
 import { secondary, background } from '../../utils/colors';
 
-const Select = styled.select`
-  display: block;
-  margin: 0 auto;
-  font-family: ${secondFont};
-  font-weight: 600;
-  color: ${secondary};
-  background-color: ${background};
-  font-size: 1.5rem;
-  border: none;
-  padding: 0.2em 2em;
-  option {
+const DropDown = styled.div`
+  :hover li {
+    display: block;
+  }
+  ul {
+    position: relative;
+    list-style: none;
+    padding: 0;
     text-align: center;
   }
-`;
-
-const DropDownMenu = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-`;
-const DropDownItem = styled.li`
-  font-family: ${secondFont};
-  font-weight: 600;
-  font-size: 1.5rem;
-  color: ${secondary};
-  padding: 0.2em 2em;
+  li {
+    display: none;
+    width: 100%;
+    font-family: ${secondFont};
+    font-weight: 600;
+    font-size: 1.5rem;
+    color: ${secondary};
+  }
+  button {
+    width: 100%;
+    text-align: center;
+    border: none;
+    outline: none;
+    background: none;
+    font-family: ${secondFont};
+    font-weight: 600;
+    font-size: 1.5rem;
+    color: ${secondary};
+    :hover {
+      display: block;
+    }
+  }
 `;
 
 const Category = () => {
   return (
-    <DropDownMenu>
-      <DropDownItem>default</DropDownItem>
-    </DropDownMenu>
+    <DropDown>
+      <button>default</button>
+      <ul>
+        <li>default</li>
+      </ul>
+    </DropDown>
   );
 };
 
