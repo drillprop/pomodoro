38a809e46b4a0e71aae8bb5ec8a1a2a76725cc8b
drillprops <drylbartosz@gmail.com>
2019-08-01 10:49:29 +0200

    make isConfigVisible from redux state avalaible to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import GlobalStyle from './utils/GlobalStyle';
 import Interface from './components/Timer/Interface';
 import HamburgerButton from './components/HamburgerButton';
+import { useSelector } from 'react-redux';
 
 const App: React.FC = () => {
+  const isConfigVisible = useSelector((state: any) => state.isConfigVisible);
   return (
     <>
       <GlobalStyle />
       <HamburgerButton />
-      <Interface />
+      {isConfigVisible ? 'visible' : <Interface />}
     </>
   );
 };
