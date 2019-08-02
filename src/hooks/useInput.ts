7bcced03660212462bf61bc0d3655b3a3d65f9b7
drillprops <drylbartosz@gmail.com>
2019-08-02 18:20:55 +0200

    improve useInput hook

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -2,15 +2,19 @@ import { useState } from 'react';
 
 const useInput = (initial: any) => {
   const [state, setState] = useState(initial);
-  console.log(state);
 
   const updateState = (e: any) => {
-    const { name, max, min } = e.target;
-    const value = parseInt(e.target.value);
-    if (value < max && value > min) {
+    const { name, max, min, value } = e.target;
+    const spliced = parseInt(
+      value
+        .split('')
+        .splice(value.length - 2, 2)
+        .join('')
+    );
+    if (spliced < max && spliced > min) {
       console.log('good');
-      setState({ ...state, [name]: value });
-    } else if (value > max) {
+      setState({ ...state, [name]: spliced });
+    } else if (spliced > max) {
       setState({ ...state, [name]: parseInt(max) });
     } else {
       setState({ ...state, [name]: parseInt(min) });
