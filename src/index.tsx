712163eb5c48f183c66854d568abe91831c38cab
drillprops <drylbartosz@gmail.com>
2019-08-03 18:44:13 +0200

    change path of App

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/App';
 import store from './duck/store';
 
 const app = document.getElementById('app');
