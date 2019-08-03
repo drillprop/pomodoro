f9baa0d803d4ee1e9b6c1527337f3c76d979e616
drillprops <drylbartosz@gmail.com>
2019-08-03 12:31:21 +0200

    clean unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import GlobalStyle from './utils/GlobalStyle';
-import Interface from './components/Timer/Interface';
 import HamburgerButton from './components/HamburgerButton';
 import { useSelector } from 'react-redux';
-import Config from './components/Config/Config';
 import Router from './components/Router';
 
 const App: React.FC = () => {
