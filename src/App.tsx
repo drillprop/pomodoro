e511ea3df6bfd5b59e3cd895fdc26b17598d99df
drillprops <drylbartosz@gmail.com>
2019-08-03 13:43:47 +0200

    move redux selector to Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import * as React from 'react';
 import GlobalStyle from './utils/GlobalStyle';
 import HamburgerButton from './components/Menu/HamburgerButton';
 import { useSelector } from 'react-redux';
-import Menu from './components/Menu/Menu';
 import Router from './Router';
 
 const App: React.FC = () => {
-  const isMenuVisible = useSelector((state: any) => state.isMenuVisible);
   return (
     <>
       <GlobalStyle />
       <HamburgerButton />
-      {isMenuVisible && <Menu />}
       <Router />
     </>
   );
--- src/Router.tsx
+++ src/Router.tsx
@@ -3,11 +3,13 @@ import { HashRouter, Route } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Config from './components/Config/Config';
 import Interface from './components/Timer/Interface';
+import { useSelector } from 'react-redux';
 
 const Router = () => {
+  const isMenuVisible = useSelector((state: any) => state.isMenuVisible);
   return (
     <HashRouter>
-      <Route component={Menu} />
+      {isMenuVisible && <Route component={Menu} />}
       <Route path='/config' component={Config} />
       <Route exact path='/' component={Interface} />
     </HashRouter>
