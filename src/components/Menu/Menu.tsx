77c23e59d57c643931a16eb7f661b6040a211a0f
drillprops <drylbartosz@gmail.com>
2019-08-04 13:01:24 +0200

    make stylings for Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { primary, background, secondaryBackground } from '../../utils/colors';
+import Profile from './Profile';
+import { secondFont, primFont } from '../../utils/fonts';
 
 const StyledNavigation = styled.nav`
-  position: absolute;
+  position: fixed;
+  z-index: 5;
   top: 0;
+  height: 100%;
+  width: 400px;
+  background-color: ${primary};
+`;
+
+const LinkList = styled.ul`
+  margin-top: 50px;
+  padding: 0;
+  margin-left: 50px;
+  li,
+  a {
+    color: ${secondaryBackground};
+  }
+  li {
+    margin-bottom: 10px;
+    opacity: 0.6;
+    font-family: ${secondFont};
+    font-size: 16px;
+    font-weight: 400;
+    :hover {
+      opacity: 1;
+    }
+  }
 `;
 
 const Menu = (props: any) => {
@@ -12,7 +39,9 @@ const Menu = (props: any) => {
   return (
     <>
       <StyledNavigation>
-        <ul>
+        <Profile />
+        <LinkList>
+          <li>profile</li>
           <li>
             {pathname === '/config' ? (
               <Link to='/'>back to app</Link>
@@ -20,7 +49,9 @@ const Menu = (props: any) => {
               <Link to='/config'>config</Link>
             )}
           </li>
-        </ul>
+          <li>stats</li>
+          <li>logout</li>
+        </LinkList>
       </StyledNavigation>
     </>
   );
