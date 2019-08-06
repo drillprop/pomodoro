49d7e230f45a7634071ed81f48196524b67a0d48
drillprops <drylbartosz@gmail.com>
2019-08-06 09:16:31 +0200

    create hook which will keep only reduxState and return it

---
+++ src/hooks/useTimerState.ts
@@ -0,0 +1,14 @@
+import { useSelector } from 'react-redux';
+import { convertSecToStr } from '../utils/helpers';
+
+export default () => {
+  const reduxState = useSelector((state: any) => state);
+
+  const seconds = reduxState.isInterval
+    ? reduxState.intervalTime
+    : reduxState.breakTime;
+
+  const timeAsString = convertSecToStr(seconds);
+
+  return { ...reduxState, timeAsString };
+};
