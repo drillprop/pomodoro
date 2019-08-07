cb446d753848e1f7b7dbb5ee3085edf979f94622
drillprops <drylbartosz@gmail.com>
2019-08-07 10:15:40 +0200

    remove dispatch methods

--- src/hooks/useTimerMethods.ts
+++ src/hooks/useTimerMethods.ts
@@ -1,32 +1,13 @@
 import { useDispatch } from 'react-redux';
 import useTimerState from './useTimerState';
-import {
-  updateTimer,
-  startPauseTimer,
-  resetRetryTimer,
-  switchCategory,
-  switchFaze
-} from '../duck/timer/timerActions';
+import { updateTimer, switchFaze } from '../duck/timer/timerActions';
 
 export default () => {
   const dispatch = useDispatch();
-  const { seconds, isInterval, isTimerStart } = useTimerState();
-
-  //DISPATCHERS
-  const retry = () => {
-    dispatch(resetRetryTimer(isTimerStart));
-  };
-
-  const switchCtg = (categoryName: string) => {
-    dispatch(switchCategory(categoryName));
-  };
+  const { isInterval } = useTimerState();
 
   const fazeSwitch = () => {
     dispatch(switchFaze(isInterval));
-  };
-
-  const startPause = () => {
-    dispatch(startPauseTimer(isTimerStart));
   };
 
   const updateSeconds = (num: number) => {
@@ -37,13 +18,5 @@ export default () => {
     }
   };
 
-  const reset = () => {
-    dispatch(resetRetryTimer(false));
-    if (!isInterval) {
-      updateSeconds(0);
-      fazeSwitch();
-    }
-  };
-
-  return { startPause, reset, retry, switchCtg, fazeSwitch, updateSeconds };
+  return { fazeSwitch, updateSeconds };
 };
