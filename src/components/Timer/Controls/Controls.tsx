5ad7cc835ba4e294b0e38b8a449e83feb5558b66
drillprops <drylbartosz@gmail.com>
2019-08-08 18:22:32 +0200

    remove unused imports

--- src/components/Timer/Controls/Controls.tsx
+++ src/components/Timer/Controls/Controls.tsx
@@ -1,13 +1,5 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import {
-  secondaryBackground,
-  primary,
-  background
-} from '../../../utils/colors';
-import Icon from '../../../elements/Icon';
-import useTimerState from '../../../hooks/useTimerState';
-import useTimerMethods from '../../../hooks/useTimerMethods';
 import ResetButton from './ResetButton';
 import RetryButton from './RetryButton';
 import PlayButton from './PlayButton';
