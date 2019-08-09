41383c4d2123c87c472b3c767f96b1ca8af776e9
drillprops <drylbartosz@gmail.com>
2019-08-09 12:18:08 +0200

    move every input inside Config

--- src/components/Config/Config.tsx
+++ src/components/Config/Config.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import { secondFont } from '../../utils/fonts';
-import { primary } from '../../utils/colors';
+import { secondFont, primFont } from '../../utils/fonts';
+import { primary, background, secondary } from '../../utils/colors';
 import ConfigForm from './ConfigForm';
 import { Link } from 'react-router-dom';
-import { MainTitle } from '../../elements/Titles';
+import { MainTitle, SubTitle } from '../../elements/Titles';
+import { SubmitButtom } from '../../elements/Forms';
 
 const ConfigWrapper = styled.main`
   display: grid;
@@ -19,12 +20,73 @@ const GoBackLink = styled.h4`
   margin-right: 16px;
 `;
 
+const StyledConfigInput = styled.input`
+  font-family: ${primFont};
+  font-size: 20px;
+  margin: 0 auto;
+  width: 70px;
+  padding: 8px;
+  color: ${primary};
+  background-color: ${background};
+  border: solid 1px ${secondary};
+  border-radius: 5px;
+  margin-left: 16px;
+`;
+const StyledConfigLabel = styled.label`
+  font-size: 1.5rem;
+  margin-right: 20px;
+`;
+
 const Config: FC = () => {
   return (
     <ConfigWrapper>
       <MainTitle>config</MainTitle>
-      <ConfigForm formName='interval' />
-      <ConfigForm formName='break' />
+      <form>
+        <SubTitle>interval time</SubTitle>
+        <StyledConfigLabel htmlFor={`interval-min`}>
+          <StyledConfigInput
+            type='number'
+            name='minutes'
+            min={0}
+            max={999}
+            id={`interval-min`}
+          />{' '}
+          min
+        </StyledConfigLabel>
+        <StyledConfigLabel htmlFor={`interval-sec`}>
+          <StyledConfigInput
+            type='number'
+            name='seconds'
+            min={0}
+            max={59}
+            id={`interval-sec`}
+          />{' '}
+          sec
+        </StyledConfigLabel>
+
+        <SubTitle>break time</SubTitle>
+        <StyledConfigLabel htmlFor={`break-min`}>
+          <StyledConfigInput
+            type='number'
+            name='minutes'
+            min={0}
+            max={999}
+            id={`break-min`}
+          />{' '}
+          min
+        </StyledConfigLabel>
+        <StyledConfigLabel htmlFor={`break-sec`}>
+          <StyledConfigInput
+            type='number'
+            name='seconds'
+            min={0}
+            max={59}
+            id={`break-sec`}
+          />{' '}
+          sec
+        </StyledConfigLabel>
+        <SubmitButtom type='submit'>save</SubmitButtom>
+      </form>
       <GoBackLink>
         <Link to='/'>back</Link>
       </GoBackLink>
