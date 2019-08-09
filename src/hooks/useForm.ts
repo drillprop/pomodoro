13ff9431a070d792901a415e28697687a0ca5981
drillprops <drylbartosz@gmail.com>
2019-08-09 18:29:49 +0200

    upgrade useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { convertSecToObj } from '../utils/helpers';
+import { useState } from 'react';
 
 export default () => {
   const initialSeconds = useSelector(
@@ -14,5 +15,39 @@ export default () => {
     };
   }, {});
 
-  console.log(reducedToObj);
+  const [timeleft, setTimeleft] = useState(reducedToObj);
+
+  const updateState = (e: any) => {
+    let { name, max, min, value } = e.target;
+
+    e.target.value = '';
+    const maxLength = name.includes('min') ? 3 : 2;
+
+    const spliced = parseInt(
+      value
+        .split('')
+        .slice(-maxLength)
+        .join('')
+    );
+    const faze = name.split('-')[0];
+    const propertyName = name.includes('min') ? 'minutes' : 'seconds';
+
+    if (spliced <= max && spliced >= min) {
+      setTimeleft({
+        ...timeleft,
+        [faze]: { ...timeleft[faze], [propertyName]: spliced }
+      });
+    } else if (spliced > max) {
+      setTimeleft({
+        ...timeleft,
+        [faze]: { ...timeleft[faze], [propertyName]: parseInt(max) }
+      });
+    } else {
+      setTimeleft({
+        ...timeleft,
+        [faze]: { ...timeleft[faze], [propertyName]: parseInt(min) }
+      });
+    }
+  };
+  return [timeleft, updateState];
 };
