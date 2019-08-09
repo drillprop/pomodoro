6493f64a692534b11ecdea081d4ef9339d252049
drillprops <drylbartosz@gmail.com>
2019-08-09 19:01:32 +0200

    replace handleSubmit with useForm submit

--- src/components/Config/Config.tsx
+++ src/components/Config/Config.tsx
@@ -1,13 +1,11 @@
-import React, { FC, FormEvent } from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
 import { secondFont } from '../../utils/fonts';
 import ConfigForm from './ConfigFazeInputs';
 import { Link } from 'react-router-dom';
 import { MainTitle } from '../../elements/Titles';
 import { SubmitButtom } from '../../elements/Forms';
-import { useDispatch } from 'react-redux';
 import useForm from '../../hooks/useForm';
-import { setTimers } from '../../duck/timer/timerActions';
 
 const ConfigWrapper = styled.main`
   display: grid;
@@ -23,26 +21,12 @@ const GoBackLink = styled.h4`
 `;
 
 const Config: FC = () => {
-  const [timeleft, update] = useForm();
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
-    const intervalSeconds =
-      timeleft.intervalTime.seconds + timeleft.intervalTime.minutes * 60;
-
-    const breakSeconds =
-      timeleft.breakTime.seconds + timeleft.breakTime.minutes * 60;
-
-    dispatch(setTimers(intervalSeconds, 'intervalTime'));
-    dispatch(setTimers(breakSeconds, 'breakTime'));
-  };
+  const [timeleft, update, submit] = useForm();
 
   return (
     <ConfigWrapper>
       <MainTitle>config</MainTitle>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={submit}>
         <ConfigForm faze='intervalTime' update={update} timeleft={timeleft} />
         <ConfigForm faze='breakTime' update={update} timeleft={timeleft} />
         <SubmitButtom type='submit'>save</SubmitButtom>
