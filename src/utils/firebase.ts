55d81c2e66a214939de77a14a45036cf4c82bba3
drillprops <drylbartosz@gmail.com>
2019-08-19 11:45:01 +0200

    create firebase config file and initialize firebase application

---
+++ src/utils/firebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGEING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+const app = firebase.initializeApp(firebaseConfig);
