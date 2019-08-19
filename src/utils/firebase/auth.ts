c4b6c1e4eaa2bb22d29519218d8c3da44a16c6b8
drillprops <drylbartosz@gmail.com>
2019-08-19 13:40:01 +0200

    create createUser method and add it to auth, create logout inside auth

--- src/utils/firebase/auth.ts
+++ src/utils/firebase/auth.ts
@@ -1,12 +1,14 @@
 import * as firebase from 'firebase/app';
 import { auth } from './firebase';
+import { createUser } from './firestore';
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
 provider.setCustomParameters({ prompt: 'select_account' });
 
 export const loginWithGoogle = () => auth.signInWithPopup(provider);
+export const logout = () => auth.signOut();
 
 export const user = auth.onAuthStateChanged(usr => {
-  console.log(usr);
+  createUser(usr);
 });
--- src/utils/firebase/firestore.ts
+++ src/utils/firebase/firestore.ts
@@ -1,1 +1,25 @@
 import { firestore } from './firebase';
+
+export const createUser = async (usr: any) => {
+  if (!usr) return;
+
+  const userRef = firestore.doc(`users/${usr.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = usr;
+    const createdAt = new Date();
+
+    try {
+      userRef.set({
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return userRef;
+};
