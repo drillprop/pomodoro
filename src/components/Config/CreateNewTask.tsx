a0eced75477f10ebb12bd7aaa874f7e3b28f2e8a
drillprops <drylbartosz@gmail.com>
2019-08-19 10:18:13 +0200

    replace previous form with EditCreateTask element

--- src/components/Config/CreateNewTask.tsx
+++ src/components/Config/CreateNewTask.tsx
@@ -1,47 +1,12 @@
-import React, { ChangeEvent } from 'react';
-import styled from 'styled-components';
-import { primFont, secondFont } from '../../utils/fonts';
-import {
-  secondary,
-  background,
-  secondaryBackground,
-  primary
-} from '../../utils/colors';
+import React from 'react';
 import useSubmitTask from '../../hooks/useSubmitTask';
-
-const StyledFormCreateTask = styled.form`
-  border: solid 1px ${secondary};
-  display: grid;
-  grid-template-columns: 1fr 140px;
-  border-radius: 5px;
-  margin-bottom: 16px;
-
-  input {
-    padding: 10px 12px;
-    background-color: ${background};
-    font-family: ${primFont};
-    color: ${primary};
-    font-size: 20px;
-    border: none;
-  }
-  button {
-    border: none;
-    width: 100%;
-    cursor: pointer;
-    background: ${secondaryBackground};
-    font-family: ${secondFont};
-    color: ${primary};
-    font-weight: 700;
-    text-transform: lowercase;
-    border-radius: 5px;
-  }
-`;
+import { EditCreateTask } from '../../elements/Forms';
 
 const CreateNewTask = () => {
   const [input, handleInput, handleSubmit] = useSubmitTask('');
 
   return (
-    <StyledFormCreateTask onSubmit={handleSubmit}>
+    <EditCreateTask onSubmit={handleSubmit}>
       <input
         maxLength={20}
         value={input}
@@ -50,7 +15,7 @@ const CreateNewTask = () => {
         onChange={handleInput}
       />
       <button type='submit'>+ add task</button>
-    </StyledFormCreateTask>
+    </EditCreateTask>
   );
 };
 
