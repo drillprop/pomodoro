2b622512cd5ec50177bd604bc5042cff8efc14ae
drillprops <drylbartosz@gmail.com>
2019-08-19 14:18:35 +0200

    simplify tasks reducer

--- src/duck/tasks/tasksReducers.ts
+++ src/duck/tasks/tasksReducers.ts
@@ -1,32 +1,16 @@
 import { CREATE_TASK, EDIT_TASK, DELETE_TASK } from './tasksTypes';
 
-interface InitialTimeleft {
-  intervalTime: number;
-  breakTime: number;
-}
-
-interface Config {
-  initialTimeleft: InitialTimeleft;
-  tasks: Array<string>;
-}
-
-interface Timer {
-  config: Config;
+interface Tasks {
   tasks: any;
   selectedTask: string;
 }
 
-const config: Config = {
-  initialTimeleft: {
-    intervalTime: 5,
-    breakTime: 2
+const initialState: Tasks = {
+  tasks: {
+    default: 0,
+    study: 0,
+    work: 0
   },
-  tasks: ['default', 'study', 'work']
-};
-
-const initialState: Timer = {
-  config,
-  tasks: config.tasks.reduce((acc, ctg) => ({ ...acc, [ctg]: 0 }), {}),
   selectedTask: 'default'
 };
 
@@ -35,10 +19,6 @@ export default (state = initialState, action: any) => {
     case CREATE_TASK:
       return {
         ...state,
-        config: {
-          ...state.config,
-          tasks: [...state.config.tasks, action.taskName]
-        },
         tasks: {
           ...state.tasks,
           [action.taskName]: 0
@@ -46,19 +26,12 @@ export default (state = initialState, action: any) => {
       };
     case EDIT_TASK:
       const { prevTask, newTask } = action;
-      const { tasks } = state.config;
-      const newTasksArray = [...tasks];
-      newTasksArray[tasks.indexOf(prevTask)] = newTask;
 
       const newTasksObject = { ...state.tasks };
       if (prevTask !== 'default') delete newTasksObject[prevTask];
 
       return {
         ...state,
-        config: {
-          ...state.config,
-          tasks: [...newTasksArray]
-        },
         tasks: { ...newTasksObject, [newTask]: state.tasks[prevTask] }
       };
     case DELETE_TASK: {
@@ -68,13 +41,6 @@ export default (state = initialState, action: any) => {
       if (taskName !== 'default') delete newTasksState[taskName];
       return {
         ...state,
-        config: {
-          ...state.config,
-          tasks:
-            taskName !== 'default'
-              ? state.config.tasks.filter(task => task !== taskName)
-              : state.config.tasks
-        },
         tasks: newTasksState
       };
     }
