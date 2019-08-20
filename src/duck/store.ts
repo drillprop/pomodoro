2f4c529ed17ce1e9221e3ed7b6c585c2fc036daf
drillprops <drylbartosz@gmail.com>
2019-08-20 14:55:31 +0200

    rename reducers

--- src/duck/store.ts
+++ src/duck/store.ts
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import timerReducer from './timer/timerReducer';
-import tasksReducer from './tasks/tasksReducers';
-import usersReducers from './users/userReducer';
+import tasksReducer from './tasks/tasksReducer';
+import usersReducer from './users/userReducer';
 
-const reducer = combineReducers({ timerReducer, tasksReducer, usersReducers });
+const reducer = combineReducers({ timerReducer, tasksReducer, usersReducer });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
