8536bf2c856252dc5164f19fb3811105717841a7
drillprops <drylbartosz@gmail.com>
2019-08-20 20:09:34 +0200

    create registerAccount method

--- src/utils/firebase/auth.ts
+++ src/utils/firebase/auth.ts
@@ -8,6 +8,21 @@ provider.setCustomParameters({ prompt: 'select_account' });
 
 export const loginWithGoogle = () => auth.signInWithPopup(provider);
 
+export const registerAccount = async (obj: any) => {
+  const { email, password, displayName } = obj;
+  try {
+    const newUser = await auth.createUserWithEmailAndPassword(email, password);
+    const user = await newUser.user;
+    if (user) {
+      user.updateProfile({
+        displayName
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const logout = async () => {
   try {
     await auth.signOut();
