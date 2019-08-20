e3db2515aca0a967a787a82d7ee16044aea410a9
drillprops <drylbartosz@gmail.com>
2019-08-20 22:02:18 +0200

    dispatch register when submit

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,16 +1,20 @@
 import { useState, ChangeEvent } from 'react';
-import { registerAccount } from '../utils/firebase/auth';
+import { useDispatch } from 'react-redux';
+import { register } from '../duck/users/userActions';
 
 export default (initValues: any = {}): [any, any, any] => {
+  const dispatch = useDispatch();
+
   const [values, setForm] = useState(initValues);
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setForm({ ...values, [e.target.name]: e.target.value });
   };
 
-  const submitForm = (e: any) => {
+  const submitForm = async (e: any) => {
     e.preventDefault();
-    registerAccount({ ...values });
+    dispatch(register({ ...values }));
   };
+
   return [values, handleInput, submitForm];
 };
