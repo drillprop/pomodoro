2f77499accac41ce83439f241afcec7c75d58c08
drillprops <drylbartosz@gmail.com>
2019-08-21 11:21:18 +0200

    remove dispatch, and register action from useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,10 +1,6 @@
 import { useState, ChangeEvent } from 'react';
-import { useDispatch } from 'react-redux';
-import { register } from '../duck/users/userActions';
 
 export default (initValues: any = {}): [any, any, any] => {
-  const dispatch = useDispatch();
-
   const [values, setForm] = useState(initValues);
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
@@ -13,7 +9,6 @@ export default (initValues: any = {}): [any, any, any] => {
 
   const submitForm = async (e: any) => {
     e.preventDefault();
-    dispatch(register({ ...values }));
   };
 
   return [values, handleInput, submitForm];
