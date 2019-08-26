b24aadb5063fa0d2a6c25b6940c01c2a0702cf01
drillprops <drylbartosz@gmail.com>
2019-08-26 18:28:07 +0200

    add redirection if user is sign in or sign out

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Config from '../pages/config/Config';
 import Interface from '../pages/timer/Interface';
 import Sign from '../pages/sign/Sign';
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../duck/store';
 
 const Router = () => {
+  const user = useSelector(({ user }: ReduxState) => user.user);
   return (
     <>
-      <Route path='/config' component={Config} />
-      <Route path='/sign' component={Sign} />
-      <Route exact path='/' component={Interface} />
+      <Route
+        path='/config'
+        render={() => (user ? <Config /> : <Redirect to='/sign' />)}
+      />
+      <Route
+        path='/sign'
+        render={() => (!user ? <Sign /> : <Redirect to='/' />)}
+      />
+      <Route
+        exact
+        path='/'
+        render={() => (user ? <Interface /> : <Redirect to='/sign' />)}
+      />
     </>
   );
 };
