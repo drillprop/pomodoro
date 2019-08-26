e51d0f62a926069462fd74ffde902b997488c6b5
drillprops <drylbartosz@gmail.com>
2019-08-26 19:46:51 +0200

    disable switchFaze by commenting it in useTimer

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import useTimerState from './useTimerState';
 import { countTimeLeft } from '../utils/helpers';
-import { switchFaze } from '../duck/timer/timerActions';
+// import { switchFaze } from '../duck/timer/timerActions';
 
 export default () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const { isTimerStart, isInterval, endTime, timeleft } = useTimerState();
 
   const initTime: number = endTime
@@ -28,7 +28,7 @@ export default () => {
     if (count <= 0 && isTimerStart) {
       setCount(0);
       clearTimeout(timeoutTimer);
-      timeoutSwitch = setTimeout(() => dispatch(switchFaze(isInterval)), 1000);
+      // timeoutSwitch = setTimeout(() => dispatch(switchFaze(isInterval)), 1000);
     }
 
     if (!isTimerStart) setCount(timeleft);
