19d312d8642dda896f2aba6c0f16050235cef368
drillprops <drylbartosz@gmail.com>
2019-09-12 13:22:26 +0200

    add stats actions to statsReducer

--- src/duck/stats/statsReducer.ts
+++ src/duck/stats/statsReducer.ts
@@ -1,6 +1,20 @@
-export default (state: any, action: any) => {
+import {
+  FETCH_INTERVALS_BY_DAY_FAILURE,
+  FETCH_INTERVALS_BY_DAY_START,
+  FETCH_INTERVALS_BY_DAY_SUCCESS
+} from './statsTypes';
+
+const initalState = {
+  intervalsByDay: null
+};
+
+export default (state = initalState, action: any) => {
   switch (action.type) {
-    case 'SMTH':
+    case FETCH_INTERVALS_BY_DAY_START:
+      return { ...state };
+    case FETCH_INTERVALS_BY_DAY_SUCCESS:
+      return { ...state };
+    case FETCH_INTERVALS_BY_DAY_FAILURE:
       return { ...state };
     default:
       return {
