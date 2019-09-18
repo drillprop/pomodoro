0ed04783af0f1247f6367b059b2d4cc03d2318ff
drillprops <drylbartosz@gmail.com>
2019-09-18 18:27:49 +0200

    add redux saga middleware to store

--- src/duck/store.ts
+++ src/duck/store.ts
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleWare from 'redux-saga';
 import thunk from 'redux-thunk';
 import timerReducer, { TimerState } from './timer/timerReducer';
 import tasksReducer, { TasksState } from './tasks/tasksReducer';
 import userReducer, { UserState } from './users/userReducer';
 import statsReducer from './stats/statsReducer';
+
+const sagaMiddleware = createSagaMiddleWare();
 
 const reducer = combineReducers({
   timer: timerReducer,
@@ -13,12 +16,17 @@ const reducer = combineReducers({
   stats: statsReducer
 });
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, sagaMiddleware))
+);
 
 export interface ReduxState {
   timer: TimerState;
   tasks: TasksState;
   user: UserState;
 }
+
+// sagaMiddleware.run();
 
 export default store;
