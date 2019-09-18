5755c3c7f68859e4ccf9d8ea493f46d0f37f8a61
drillprops <drylbartosz@gmail.com>
2019-09-18 12:57:00 +0200

    move Spinner and it's wrapper to another newly created component Loading

--- src/components/withLoading.tsx
+++ src/components/withLoading.tsx
@@ -1,60 +1,6 @@
 import React, { FC } from 'react';
-import styled from 'styled-components';
-import { primary } from '../utils/colors';
 import { useTransition, animated } from 'react-spring';
-
-const LoadingWrapper = styled.main`
-  display: grid;
-  height: 80vh;
-  align-content: center;
-  justify-content: center;
-`;
-
-const Spinner = styled.div`
-  background: ${primary};
-  animation: load1 1s infinite ease-in-out;
-  width: 1em;
-  height: 4em;
-
-  ::before,
-  ::after {
-    background: ${primary};
-    animation: load1 1s infinite ease-in-out;
-    width: 1em;
-    height: 4em;
-  }
-
-  margin: 88px auto;
-  position: relative;
-  transform: translateZ(0);
-  animation-delay: -0.16s;
-
-  ::before,
-  ::after {
-    position: absolute;
-    top: 0;
-    content: '';
-  }
-  ::before {
-    left: -1.5em;
-    animation-delay: -0.32s;
-  }
-  ::after {
-    left: 1.5em;
-  }
-  @keyframes load1 {
-    0%,
-    80%,
-    100% {
-      box-shadow: 0 0;
-      height: 4em;
-    }
-    40% {
-      box-shadow: 0 -2em;
-      height: 5em;
-    }
-  }
-`;
+import Loading from '../elements/Loading';
 
 const withLoading = (Component: FC) => ({ isLoading, ...otherProps }: any) => {
   const transitions = useTransition(isLoading, null, {
@@ -67,9 +13,7 @@ const withLoading = (Component: FC) => ({ isLoading, ...otherProps }: any) => {
       {transitions.map(({ item, key, props }) =>
         item ? (
           <animated.div style={props} key={key}>
-            <LoadingWrapper>
-              <Spinner />
-            </LoadingWrapper>
+            <Loading />
           </animated.div>
         ) : (
           <animated.div style={props} key={key}>
---
+++ src/elements/Loading.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import { primary } from '../utils/colors';
+
+const LoadingWrapper = styled.main`
+  display: grid;
+  height: 80vh;
+  align-content: center;
+  justify-content: center;
+`;
+
+const Spinner = styled.div`
+  background: ${primary};
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+
+  ::before,
+  ::after {
+    background: ${primary};
+    animation: load1 1s infinite ease-in-out;
+    width: 1em;
+    height: 4em;
+  }
+
+  margin: 88px auto;
+  position: relative;
+  transform: translateZ(0);
+  animation-delay: -0.16s;
+
+  ::before,
+  ::after {
+    position: absolute;
+    top: 0;
+    content: '';
+  }
+  ::before {
+    left: -1.5em;
+    animation-delay: -0.32s;
+  }
+  ::after {
+    left: 1.5em;
+  }
+  @keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
+`;
+
+const Loading = () => {
+  return (
+    <LoadingWrapper>
+      <Spinner />
+    </LoadingWrapper>
+  );
+};
+
+export default Loading;
