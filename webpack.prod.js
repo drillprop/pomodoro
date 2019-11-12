3a90a42bda74b3fd6542d19039a425d7d077e662
drillprops <drylbartosz@gmail.com>
2019-11-12 20:51:47 +0100

    add webpack bundle analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const common = require('./webpack.common');
 const webpackMerge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
+const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 module.exports = webpackMerge(common, {
   mode: 'production',
@@ -8,5 +10,6 @@ module.exports = webpackMerge(common, {
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()]
-  }
+  },
+  plugins: [new WebpackBundleAnalyzer()]
 });
