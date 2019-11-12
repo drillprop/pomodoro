ca7038569a6d85c92883218b60c4d35f2a448edd
drillprops <drylbartosz@gmail.com>
2019-11-12 18:23:15 +0100

    add Terser Plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,12 @@
 const common = require('./webpack.common');
 const webpackMerge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = webpackMerge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
 });
