5667ba8887ca36d4d83239aaa21c7068a7e5150a
drillprops <drylbartosz@gmail.com>
2019-11-20 18:11:20 +0100

    show spinner if is fetching user

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkSession } from '../../duck/users/userActions';
 import GlobalStyle from '../../utils/GlobalStyle';
 import HamburgerButton from './app/menu/HamburgerButton';
 import Menu from './app/Menu';
 import Router from './app/Router';
 import ProgressBar from '../ProgressBar/ProgressBar';
+import { ReduxState } from '../../duck/store';
+import Loading from '../Loading/Loading';
 
 const App: React.FC = () => {
+  const isGettingUser = useSelector(
+    ({ user }: ReduxState) => user.isGettingUser
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,7 +25,7 @@ const App: React.FC = () => {
       <HamburgerButton />
       <Menu />
       <GlobalStyle />
-      <Router />
+      {isGettingUser ? <Loading /> : <Router />}
     </>
   );
 };
