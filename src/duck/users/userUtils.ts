3eef658fd6a8d8df0eb2f1e00cc8182eb8501e30
drillprops <drylbartosz@gmail.com>
2019-11-27 18:46:50 +0100

    remove commented code

--- src/duck/users/userUtils.ts
+++ src/duck/users/userUtils.ts
@@ -31,36 +31,3 @@ export const getConfigAndTasks = async (uid: string) => {
     return error;
   }
 };
-
-// import { firestore } from '../../utils/firebase/firebase';
-
-// export const addUserToFirestore = async (user: any) => {
-//   if (!user) return;
-
-//   // const userRef = firestore.doc(`users/${user.uid}`);
-//   const snapShot = await userRef.get();
-//   if (!snapShot.exists) {
-//     const { displayName, email, uid } = user;
-//     const createdAt = new Date();
-
-//     try {
-//       await userRef.set({
-//         displayName,
-//         email,
-//         uid,
-//         createdAt,
-//         config: {
-//           breakTime: 60 * 10,
-//           intervalTime: 60 * 30
-//         },
-//         tasks: {
-//           default: 0
-//         },
-//         selectedTask: 'default'
-//       });
-//     } catch (err) {
-//       console.log('error creating user', err.message);
-//     }
-//   }
-//   return userRef;
-// };
