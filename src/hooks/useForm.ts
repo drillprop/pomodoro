6bc8b665f46e342b2ac33db1e5818acac926893f
drillprops <drylbartosz@gmail.com>
2019-12-02 12:34:25 +0100

    add more proper types in useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,13 +1,19 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 
-export default (initValues: any = {}): [any, any, any] => {
+export default (
+  initValues: any = {}
+): [
+  any,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  (e: FormEvent<HTMLFormElement>) => Promise<void>
+] => {
   const [values, setForm] = useState(initValues);
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setForm({ ...values, [e.target.name]: e.target.value });
   };
 
-  const submitForm = async (e: any) => {
+  const submitForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
