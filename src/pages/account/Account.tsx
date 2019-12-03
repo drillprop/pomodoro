f91109b101f53fd21d2d4dd91d469fc47a15e039
drillprops <drylbartosz@gmail.com>
2019-12-03 10:56:16 +0100

    hide change password form if login provider were google

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { MainTitle } from '../../elements/Titles';
 import { AccountWrapper } from './Account.styles';
 import ChangePassword from './account/ChangePassword';
+import { useSelector } from 'react-redux';
+import { selectProvider } from '../../duck/users/userSelectors';
 
 const Account = () => {
+  const provider = useSelector(selectProvider);
   return (
     <AccountWrapper>
       <MainTitle>account</MainTitle>
-      <ChangePassword />
+      {provider === 'password' && <ChangePassword />}
       <p>Reset Your Stats</p>
       <p>Delete Account</p>
     </AccountWrapper>
