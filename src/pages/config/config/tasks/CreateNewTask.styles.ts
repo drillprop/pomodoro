0ca3bf38be5d9573bba7aa11360638ae3fec82af
drillprops <drylbartosz@gmail.com>
2019-12-06 19:42:53 +0100

    create CreateNewTask.styles and Sign.styles

---
+++ src/pages/config/config/tasks/CreateNewTask.styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import {
+  secondary,
+  background,
+  primary,
+  secondaryBackground
+} from '../../../../utils/colors';
+import { primFont, secondFont } from '../../../../utils/fonts';
+
+export const EditCreateTask = styled.form`
+  border: solid 1px ${secondary};
+  display: grid;
+  grid-template-columns: 1fr 140px;
+  border-radius: 5px;
+  margin-bottom: 16px;
+
+  input {
+    padding: 10px 12px;
+    background-color: ${background};
+    font-family: ${primFont};
+    color: ${primary};
+    font-size: 20px;
+    border: none;
+  }
+  button {
+    border: none;
+    width: 100%;
+    cursor: pointer;
+    background: ${secondaryBackground};
+    font-family: ${secondFont};
+    color: ${primary};
+    font-weight: 700;
+    text-transform: lowercase;
+    border-radius: 5px;
+  }
+`;
---
+++ src/pages/sign/Sign.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FormWrapper = styled.main`
+  display: grid;
+  margin-top: 100px;
+  grid-template-rows: 100px 1fr;
+  grid-template-columns: 300px;
+  justify-content: center;
+`;
+
+export const FormParagraph = styled.p`
+  cursor: pointer;
+`;
