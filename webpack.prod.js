1401542e2e11508d33f983a208d9630e1f24f6ca
drillprops <drylbartosz@gmail.com>
2019-12-07 11:09:17 +0100

    install clean-webpack-plugin, delete redundant webpack code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,13 @@
 const common = require('./webpack.common');
-const { EnvironmentPlugin } = require('webpack');
 const webpackMerge = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 
 module.exports = webpackMerge(common, {
   mode: 'production',
-  optimization: {
-    minimize: true
-  },
   plugins: [
-    new EnvironmentPlugin({
-      NODE_ENV: 'production',
-      DEBUG: false
-    })
+    new CleanWebpackPlugin()
     // new WebpackBundleAnalyzer()
   ]
 });
