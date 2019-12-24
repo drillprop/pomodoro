a7c047848a3f8adb048ff3da5d7ffec2a13c9741
drillprops <drylbartosz@gmail.com>
2019-12-24 17:16:13 +0100

    install compression webpack plugin and add it to production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,13 @@ const webpackMerge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 module.exports = webpackMerge(common, {
   mode: 'production',
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CompressionWebpackPlugin()
     // new WebpackBundleAnalyzer()
   ]
 });
