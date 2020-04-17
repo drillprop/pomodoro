4883bb139abcb18c868bde9e6b95fcfc4d3d7d7f
drillprops <drylbartosz@gmail.com>
2020-04-17 17:38:06 +0200

    add Props interface to TomatoIcon and StyledSvg

--- src/components/TomatoIcon/TomatoIcon.styles.ts
+++ src/components/TomatoIcon/TomatoIcon.styles.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
+import { primary } from '../../utils/colors';
 
-export const StyledSvg = styled.svg`
+interface StyledSvgProps {
+  size?: string;
+  fill?: string;
+}
+
+export const StyledSvg = styled.svg<StyledSvgProps>`
   display: block;
-  fill: #333;
+  fill: ${(props) => props.fill || primary};
   position: absolute;
   top: 130px;
   left: 50%;
   transform: translate(-50%, 0);
-  width: 500px;
+  width: ${({ size = '50px' }) => size};
   z-index: -2;
 `;
