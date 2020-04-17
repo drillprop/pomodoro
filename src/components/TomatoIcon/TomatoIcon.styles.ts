932b61f88cba71bd7ae9572f41b5a4d47c1b4c45
drillprops <drylbartosz@gmail.com>
2020-04-17 13:59:49 +0200

    create TomatoIcon component

---
+++ src/components/TomatoIcon/TomatoIcon.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledSvg = styled.svg`
+  display: block;
+  fill: #333;
+  position: absolute;
+  top: 130px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 500px;
+  z-index: -2;
+`;
