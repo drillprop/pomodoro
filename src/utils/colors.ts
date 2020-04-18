20649475d241a58293e3deb59322b3f195885777
drillprops <drylbartosz@gmail.com>
2020-04-18 22:54:28 +0200

    add reds and grays array to colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,3 +2,21 @@ export const background: string = '#F9F9F9';
 export const primary: string = '#4a4a4a';
 export const secondary: string = '#a3a3a3';
 export const secondaryBackground: string = '#d8d8d8';
+
+export const reds = [
+  '#B71C1C',
+  '#D32F2F',
+  '#E53935',
+  '#EF5350',
+  '#EF9A9A',
+  '#FFCDD2',
+];
+
+export const grays = [
+  '#212121',
+  '#424242',
+  '#757575',
+  '#9E9E9E',
+  '#BDBDBD',
+  '#E0E0E0',
+];
