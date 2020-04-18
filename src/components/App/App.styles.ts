4d949f42feeccac968fe0a915ce693f379ba4383
drillprops <drylbartosz@gmail.com>
2020-04-18 23:22:48 +0200

    change position of timer

--- src/components/App/App.styles.ts
+++ src/components/App/App.styles.ts
@@ -4,5 +4,5 @@ export const Layout = styled.div`
   display: grid;
   grid-template-columns: 300px;
   justify-content: center;
-  margin-top: 150px;
+  margin-top: 135px;
 `;
--- src/pages/timer/interface/Timer.styles.ts
+++ src/pages/timer/interface/Timer.styles.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import { background, grays } from '../../../utils/colors';
 
 export const TimerWrapper = styled.section`
-  margin-top: 150px;
+  margin-top: 170px;
   text-align: center;
 `;
 
 export const Time = styled.h1`
-  padding: 15px;
+  padding: 20px;
   display: inline-block;
   background-color: ${background};
   border-radius: 50% 50% 0% 0%;
