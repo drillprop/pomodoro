cb19e47a6bab3c74e7f3829a0e93026b2fa223c6
drillprops <drylbartosz@gmail.com>
2020-04-18 19:14:40 +0200

    add TimerFaze animation

--- src/pages/timer/interface/TimerFaze.tsx
+++ src/pages/timer/interface/TimerFaze.tsx
@@ -1,11 +1,31 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectIsInterval } from '../../../duck/timer/timerSelectors';
 import Heading from '../../../components/Heading/Heading';
+import { useTransition, animated } from 'react-spring';
 
-const TimerFaze: FC = () => {
+const TimerFaze: React.FC = () => {
   const isInterval = useSelector(selectIsInterval);
-  return <Heading level='h1'>{isInterval ? 'interval' : 'break'}</Heading>;
+  const transition = useTransition(isInterval, null, {
+    from: { transform: 'translate3d(0,-40px,0)', opacity: 0 },
+    enter: { transform: 'translate3d(0,0px,0)', opacity: 1 },
+    leave: { transform: 'translate3d(0,-40px,0)', height: 0, opacity: 0 },
+  });
+  return (
+    <>
+      {transition.map(({ item, key, props }) => {
+        return item ? (
+          <animated.div style={props} key={key}>
+            <Heading level='h1'>{isInterval ? 'interval' : 'break'}</Heading>
+          </animated.div>
+        ) : (
+          <animated.div style={props} key={key}>
+            <Heading level='h1'>{isInterval ? 'interval' : 'break'}</Heading>
+          </animated.div>
+        );
+      })}
+    </>
+  );
 };
 
 export default TimerFaze;
