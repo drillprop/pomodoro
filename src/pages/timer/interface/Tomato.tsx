ade1113bce98f01af2e86acfc983af477988f3db
drillprops <drylbartosz@gmail.com>
2020-04-18 17:47:55 +0200

    improve Tomato animation by adding mask and react-spring useSpring

--- src/pages/timer/interface/Tomato.tsx
+++ src/pages/timer/interface/Tomato.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import TomatoIcon from '../../../components/TomatoIcon/TomatoIcon';
+import { animated, useSpring, config } from 'react-spring';
 import { TomatoWrapper } from './Tomato.styles';
 import { useSelector } from 'react-redux';
 import { selectTimeleft } from '../../../duck/timer/timerSelectors';
@@ -10,11 +11,13 @@ interface Props {
 
 const Tomato: React.FC<Props> = ({ count }) => {
   const timeleft = useSelector(selectTimeleft);
-  const progress = Math.abs((count / timeleft) * 100 - 100);
+  const progress = Math.abs((count / timeleft) * 100 - 100) / 2;
+
+  const props = useSpring({ number: progress });
   return (
     <TomatoWrapper>
-      <TomatoIcon size='270px' fill='silver' />
-      <TomatoIcon size='270px' fill='grey' coverage={progress} />
+      <TomatoIcon size='280px' fill='silver' />
+      <TomatoIcon size='280px' fill='grey' coverage={props.number} />
     </TomatoWrapper>
   );
 };
