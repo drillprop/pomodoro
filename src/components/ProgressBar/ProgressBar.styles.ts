5ce6572ad27e78d40db3fc1387ede3cdb056abec
drillprops <drylbartosz@gmail.com>
2020-04-19 01:23:48 +0200

    change ProgressBar color to red

--- src/components/ProgressBar/ProgressBar.styles.ts
+++ src/components/ProgressBar/ProgressBar.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { primary } from '../../utils/colors';
+import { primary, reds } from '../../utils/colors';
 
 export const ProgressBarWrapper = styled.div`
   position: fixed;
@@ -12,7 +12,7 @@ export const ProgressBarWrapper = styled.div`
 export const ProgressBarLine = styled.div<{ length: number }>`
   width: 100%;
   height: 7px;
-  background-color: ${primary};
+  background-color: ${reds[0]};
   transform: ${({ length }) => `scaleX(${length})`};
   transform-origin: left;
   transition: transform ${({ length }) => (length > 0 ? 1000 : 200)}ms linear;
