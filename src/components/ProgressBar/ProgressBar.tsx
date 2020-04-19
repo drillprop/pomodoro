195feed99a98cd5470540ebfc2668ba5e9abcd93
drillprops <drylbartosz@gmail.com>
2020-04-19 18:56:11 +0200

    remove ProgressBar.styles, use svg and react-spring to animate ProgressBar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import { ProgressBarWrapper, ProgressBarLine } from './ProgressBar.styles';
-import useTimer from '../../hooks/useTimer';
 import { useSelector } from 'react-redux';
-import { selectTimeleft } from '../../duck/timer/timerSelectors';
+import { animated, useSpring } from 'react-spring';
+import {
+  selectIsTimerStart,
+  selectTimeleft,
+} from '../../duck/timer/timerSelectors';
+import useTimer from '../../hooks/useTimer';
+import { reds } from '../../utils/colors';
 
 const ProgressBar = () => {
   const timeleft = useSelector(selectTimeleft);
+  const isTimerStart = useSelector(selectIsTimerStart);
   const count = useTimer();
-  let progress = ((timeleft - count) * (100 / timeleft)) / 100;
+  const progress = Math.abs((count / timeleft) * 100 - 100);
+
+  const props = useSpring({
+    number: progress + '%',
+    config: isTimerStart
+      ? {
+          duration: 1000,
+        }
+      : {},
+  });
+
   return (
-    <ProgressBarWrapper>
-      <ProgressBarLine length={progress} />
-    </ProgressBarWrapper>
+    <svg width='100%' height='20'>
+      <animated.line
+        x1='0%'
+        x2={props.number}
+        y1='0%'
+        y2='0%'
+        stroke={reds[0]}
+        strokeWidth='10'
+      />
+    </svg>
   );
 };
 
