b0851e2df140d99fa90c9400ef01023f5589e8cf
drillprops <drylbartosz@gmail.com>
2020-04-20 10:55:31 +0200

    change Heading font-sizes

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -10,7 +10,7 @@ export const StyledH1 = styled.h1<HeadingProps>`
   margin: 0;
   margin-top: ${({ mtop }) => mtop}px;
   text-align: center;
-  font-size: 3.5rem;
+  font-size: 3rem;
   font-family: ${secondFont};
   font-weight: 600;
   color: ${grays[0]};
@@ -24,7 +24,7 @@ export const StyledH2 = styled.h2<HeadingProps>`
   margin: 0;
   margin-top: ${({ mtop }) => mtop}px;
   text-align: center;
-  font-size: 2rem;
+  font-size: 1.8rem;
   font-family: ${secondFont};
   color: ${reds[1]};
 `;
