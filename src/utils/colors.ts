c7734b1445da4ed1dfea86a6b8bca1e6b4a1a710
drillprops <drylbartosz@gmail.com>
2020-04-20 12:06:33 +0200

    delete unused colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,7 +1,4 @@
-export const background: string = '#F9F9F9';
-export const primary: string = '#4a4a4a';
-export const secondary: string = '#a3a3a3';
-export const secondaryBackground: string = '#d8d8d8';
+export const background = '#F9F9F9';
 
 export const reds = [
   '#B71C1C',
