bd567ad0a170dddfd9d18c46ac3fbf8a90d4fa8b
drillprops <drylbartosz@gmail.com>
2020-04-21 23:18:14 +0200

    fix gh pages not registering ServiceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,11 @@ if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register(
+        process.env.NODE_ENV === 'development'
+          ? '/sw.js'
+          : 'https://drillprop.github.io/pommodoro/sw.js'
+      )
       .catch((err) => console.error(err));
   });
 }
