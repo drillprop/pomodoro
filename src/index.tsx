476c07fe1db62fe836afd97c8e008c68d89b0dd0
drillprops <drylbartosz@gmail.com>
2020-04-21 21:36:50 +0200

    add to cache storage index.html

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,8 @@ ReactDOM.render(
 if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker
+      .register('/sw.js')
+      .catch((err) => console.error(err));
   });
 }
--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,15 @@
 const cacheName = 'v1';
-
 const cacheAssets = ['index.html'];
 
-const initialize = (service: ServiceWorkerGlobalScope): void => {};
+const initialize = (service: ServiceWorkerGlobalScope): void => {
+  service.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches
+        .open(cacheName)
+        .then((cache) => cache.addAll(cacheAssets))
+        .catch((err) => console.error(err))
+    );
+  });
+};
 
 initialize(self as any);
